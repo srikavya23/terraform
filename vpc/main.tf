a3fddf5bb44faffcb446437df8b3d964edb06c29
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-10-05 16:16:22 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -18,7 +18,7 @@ terraform {
 
 /*==== The VPC ======*/
 resource "aws_vpc" "vpc" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = "var.vpc_cidr"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
@@ -29,7 +29,7 @@ resource "aws_vpc" "vpc" {
 /*==== Subnets ======*/
 /* Internet gateway for the public subnet */
 resource "aws_internet_gateway" "ig" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "aws_vpc.vpc.id"
   tags = {
     Name        = "${var.environment}-igw"
     Environment = "${var.environment}"
@@ -52,7 +52,7 @@ resource "aws_nat_gateway" "nat" {
 }
 /* Public subnet */
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = "${aws_vpc.vpc.id}"
+  vpc_id                  = "aws_vpc.vpc.id"
   count                   = "${length(var.public_subnets_cidr)}"
   cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
   availability_zone       = "${element(var.availability_zones,   count.index)}"
@@ -84,7 +84,7 @@ resource "aws_route_table" "private" {
 }
 /* Routing table for public subnet */
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "aws_vpc.vpc.id"
   tags = {
     Name        = "${var.environment}-public-route-table"
     Environment = "${var.environment}"
