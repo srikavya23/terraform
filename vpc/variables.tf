8f2a0b66cf30b6fc439d975f0ac3a9fae525ef2d
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-10-05 18:27:36 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  default       = "us-east-1"
-  description   = "AWS Region"
-  type          = string
-}
-
 variable "vpc-cidr" {
   default       = "10.0.0.0/16"
   description   = "VPC CIDR Block"
