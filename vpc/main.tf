8f84082fa5edcb27d41c89e70b4aea8d1ff9ea66
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-10-05 16:17:14 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -42,7 +42,7 @@ resource "aws_eip" "nat_eip" {
 }
 /* NAT */
 resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat_eip.id}"
+  allocation_id = "aws_eip.nat_eip.id"
   subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
   depends_on    = [aws_internet_gateway.ig]
   tags = {
