104abae9c3daf789bc5236e062c9d4c675e4eba9
srikavyapendiala <srikavya@zelarsoft.com>
2021-10-06 17:04:28 +0530

    #commit

---
+++ GCP/sample/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/kavya/Downloads/shaped-manifest-327006-0ba12b5b56a8.json")
+
+  project = "shaped-manifest-327006"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
