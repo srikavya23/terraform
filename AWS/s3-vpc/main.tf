dfc36cb4435d0dffd3dbc47545e67a16128bd483
srikavyapendiala <srikavya@zelarsoft.com>
2021-10-06 18:59:21 +0530

    #commit

--- AWS/s3-vpc/main.tf
+++ AWS/s3-vpc/main.tf
@@ -1,38 +1,39 @@
 terraform {
   backend "s3" {
     bucket = "kav123"
-    key = "terraform.tfstate"
+    key    = "s3-vpc"
     region = "us-east-2"
   }
+}
 
-  provider "aws" {
-    region     = "us-east-1"
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name        = "sample"
   }
+}
 
-  resource "aws_vpc" "vpc" {
-    cidr_block = var.vpc_cidr
-    tags = {
-      Name        = "sample"
-    }
+resource "aws_subnet" "public" {
+  for_each = var.public_subnet_numbers
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
+  tags = {
+    Name        = "public-subnet"
   }
+}
 
-  resource "aws_subnet" "public" {
-    for_each = var.public_subnet_numbers
-    vpc_id            = aws_vpc.vpc.id
-    availability_zone = each.key
-    cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
-    tags = {
-      Name        = "public-subnet"
-    }
+resource "aws_subnet" "private" {
+  for_each = var.private_subnet_numbers
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
+  tags = {
+    Name        = "private-subnet"
+
   }
-
-  resource "aws_subnet" "private" {
-    for_each = var.private_subnet_numbers
-    vpc_id            = aws_vpc.vpc.id
-    availability_zone = each.key
-    cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
-    tags = {
-      Name        = "private-subnet"
-
-    }
-  }
+}
