7b8da15a5f43171836aab8491b9cc7dfa094432e
srikavyapendiala <srikavya@zelarsoft.com>
2021-10-06 15:10:52 +0530

    #commit

--- GCP/main.tf
+++ GCP/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("<NAME>.json")
+  credentials = file("6e00288e034f99f32f49975bc5766fbaa9db34dd.json")
 
   project = "shaped-manifest-327006"
   region  = "us-central1"
