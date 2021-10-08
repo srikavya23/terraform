0802c6d2b0fc4d1dbb4e65c7f7ede4fe24e42fdd
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-10-08 17:38:48 +0530

    Update main.tf

--- AWS/ec2-sample1/main.tf
+++ AWS/ec2-sample1/main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_file = .aws/credentials
+  profile                 = "default"
 }
 resource "aws_instance" "sample" {
   ami          = "ami-0eb5f3f64b10d3e0e"
