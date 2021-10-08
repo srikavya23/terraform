8d39d4c29f9b0b3d4d47f4b983212d410ee5b5ad
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-10-08 17:47:11 +0530

    Update main.tf

--- AWS/ec2-sample1/main.tf
+++ AWS/ec2-sample1/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file = /root/.aws
+  shared_credentials_file = "/root/.aws/credentials"
   profile                 = "default"
 }
 resource "aws_instance" "sample" {
